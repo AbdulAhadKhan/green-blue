73aa1d36798536be8bf88b4246956a5cc5b6e5f0
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-05 02:45:53 -0500

    Updated git ignore, file cleanup and updated peer.c for testing

--- peer.c
+++ peer.c
@@ -4,33 +4,14 @@
 
 #include <getopt.h>
 
-#include "meta.h"
+#include "utils/meta.h"
+#include "utils/arg-parser.h"
 
-port_number_t port_number = 2000;
-peer_mode_t mode = SERVER;
-
-void parse_args(int argc, char *argv[]) {
-    int opt;
-    static struct option long_options[] = {
-        {"port",    required_argument,  0,  'p'},
-        {"client",  no_argument,        0,  'c'},
-        {"server",  no_argument,        0,  's'},
-        {0,         0,                  0,  0}
-    };
-
-    while ((opt = getopt_long(argc, argv, "p:cs", long_options, NULL)) != -1) {
-        switch (opt) {
-            case 'p': port_number = atoi(optarg); break;
-            case 'c': mode = CLIENT; break;
-            case 's': mode = SERVER; break;
-            default: fprintf(stderr, "Usage: %s [-p port] [-c | -s]\n", argv[0]); exit(1);
-        }
-    }   
-}
+config_t configs;
 
 int main(int argc, char *argv[]) {
-    parse_args(argc, argv);
-    printf("Port: %d\n", port_number);
-    printf("Mode: %s\n", mode == CLIENT ? "CLIENT" : "SERVER");
-    return 0;
+    configs = parse_arguments(argc, argv);
+
+    printf("Port number: %d\n", configs.port_number);
+    printf("Mode: %s\n", configs.mode == SERVER ? "SERVER" : "CLIENT");
 }
