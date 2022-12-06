6e684d399cb35af0249be43c370a43ede5c305ee
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-06 17:17:56 -0500

    Testing blue

--- green-blue.c
+++ green-blue.c
@@ -1,6 +1,7 @@
 #include "utils/meta.h"
 #include "utils/initialize.h"
 #include "utils/socket-object.h"
+#include "utils/blue.h"
 
 config_t configs = {
     .port_number = 2000,
@@ -9,8 +10,6 @@ config_t configs = {
 
 int main(int argc, char *argv[]) {
     parse_arguments(argc, argv, &configs);
-
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_address = socket_object_init(configs.port_number);
-    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    socket_fd_t socket_fd = initialize_server(&configs);
+    start_server(socket_fd);
 }
