fcd9f70e1a93b10fdfc0167a8efd979f6749c6fc
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-07 05:30:10 -0500

    Parses CL arguments internally

--- green-blue.c
+++ green-blue.c
@@ -1,5 +1,6 @@
+#include <getopt.h>
+
 #include "utils/meta.h"
-#include "utils/initialize.h"
 #include "utils/socket-object.h"
 #include "utils/blue.h"
 
@@ -7,6 +8,28 @@ config_t configs = {
     .port_number = 2000,
     .mode = SERVER
 };
+
+static struct option long_options[] = {
+    {"port",    required_argument,  0,  'p'},
+    {"client",  no_argument,        0,  'c'},
+    {"server",  no_argument,        0,  's'},
+    {0,         0,                  0,  0}
+};
+
+int parse_arguments(int argc, char *argv[], config_t *configs) {
+    int opt;
+
+    while ((opt = getopt_long(argc, argv, "p:cs", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'p': configs->port_number = atoi(optarg); break;
+            case 'c': configs->mode = CLIENT; break;
+            case 's': configs->mode = SERVER; break;
+            default: fprintf(stderr, "Usage: %s [-p port] [-c | -s]\n", argv[0]); return -1;
+        }
+    }
+    
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     parse_arguments(argc, argv, &configs);
