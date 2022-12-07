ddc12ff90f4ca6ac4641cc2cb2532f0d815b577b
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-07 06:55:31 -0500

    Experimenting with callback to server

--- server-blue.c
+++ server-blue.c
@@ -1,7 +1,8 @@
-#include <getopt.h>
+#include <arpa/inet.h>
 
-#include "utils/meta.h"
-#include "utils/blue.h"
+#include "utils/utils.h"
+#include "utils/server-blue.h"
+#include "utils/ANSI-colors.h"
 
 port_number_t port_number = 2000;
 
@@ -23,9 +24,26 @@ int parse_arguments(int argc, char *argv[]) {
     return 0;
 }
 
+int server_callback(void *args) {
+    char buffer[1024];
+    struct sockaddr_in client_address;
+    socklen_t client_address_length = sizeof(client_address);
+    
+    getpeername(client_connection, (struct sockaddr *) &client_address, &client_address_length);
+    char *client_ip = inet_ntoa(client_address.sin_addr);
+    int client_port = ntohs(client_address.sin_port);
+    
+    while (read(client_connection, buffer, 1024) > 0) {
+        printf("%s[%s] %s:%d:%s %s\n", ANSI_COLOR_BLUE, get_current_time_as_string(), 
+               client_ip, client_port, ANSI_COLOR_RESET, buffer);
+    }
+
+    return 0;
+}
+
 int server_blue(port_number_t port_number) {
     socket_fd_t server_socket = initialize_server(port_number);
-    start_server(server_socket, NULL, NULL);
+    start_server(server_socket, server_callback, NULL);
     return 0;
 }
 
