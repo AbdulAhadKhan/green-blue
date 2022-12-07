5a6de05d3da7ffe97efb90f2e30c0ddfcdbbf3ad
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-07 02:18:49 -0500

    Added documentation

--- utils/blue.c
+++ utils/blue.c
@@ -7,6 +7,11 @@
 #include "socket-object.h"
 #include "ANSI-colors.h"
 
+/**
+ * @brief Get the current time as string
+ * 
+ * @return char* 
+ */
 char * get_current_time_as_string() {
     time_t current_time;
     char *date_time_string = malloc(100);
@@ -17,6 +22,13 @@ char * get_current_time_as_string() {
     return date_time_string;
 }
 
+/**
+ * @brief Log the connection to stdout
+ * 
+ * @param connection: connection file descriptor (int *)
+ * 
+ * @return int 
+ */
 int log_connection(int *connection) {
     struct sockaddr_in client_address;
     socklen_t client_address_length;
@@ -25,8 +37,20 @@ int log_connection(int *connection) {
     printf("%s[%s]%s Connection established with %s:%d\n",
            ANSI_COLOR_GREEN, get_current_time_as_string(), ANSI_COLOR_RESET, 
            inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+    
+    return 0;
 }
 
+/**
+ * @brief Handle the connection fork
+ * 
+ * @param server_socket: socket file descriptor (socket_fd_t)
+ * @param connection: connection file descriptor (int *)
+ * @param callback: callback function (int (*)(void *))
+ * @param args: arguments to be passed to the callback function (void *)
+ * 
+ * @return int 
+ */
 int connection_fork_handler(socket_fd_t server_socket, int *connection, int (*callback)(void *), void *args) {
     close(server_socket);
     log_connection(connection);
@@ -35,6 +59,13 @@ int connection_fork_handler(socket_fd_t server_socket, int *connection, int (*ca
     exit(0);
 }
 
+/**
+ * @brief Initialize the server
+ * 
+ * @param configs: configurations (config_t *)
+ * 
+ * @return socket_fd_t 
+ */
 socket_fd_t initialize_server(config_t *configs) {
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_address = socket_object_init(configs->port_number);
@@ -43,6 +74,15 @@ socket_fd_t initialize_server(config_t *configs) {
         server_socket : errno;
 }
 
+/**
+ * @brief Start the server and listen for connections
+ * 
+ * @param server_socket: socket file descriptor (socket_fd_t)
+ * @param callback: callback function (int (*)(void *))
+ * @param args: arguments to be passed to the callback function (void *)
+ * 
+ * @return int 
+ */
 int start_server(socket_fd_t server_socket, int (*callback)(void *), void *args) {
     int connection;
 
--- utils/blue.h
+++ utils/blue.h
@@ -6,6 +6,6 @@
 #include "socket-object.h"
 
 socket_fd_t initialize_server(config_t *configs);
-int start_server(int server_socket);
+int start_server(socket_fd_t server_socket, int (*callback)(void *), void *args);
 
 #endif
