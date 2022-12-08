61ed9dd93624da55c8bd55e1a1cca120865d1e5b
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-08 01:49:28 -0500

    Removed CL arg parsing

--- utils/server-blue.c
+++ utils/server-blue.c
@@ -1,31 +1,13 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-#include "utils/utils.h"
-#include "utils/server-blue.h"
-#include "utils/ANSI-colors.h"
+#include "utils.h"
+#include "server-blue.h"
+#include "ANSI-colors.h"
 
 port_number_t port_number = 2000;
 
-static struct option long_options[] = {
-    {"port",    required_argument,  0,  'p'},
-    {0,         0,                  0,  0}
-};
-
-int parse_arguments(int argc, char *argv[]) {
-    int opt;
-
-    while ((opt = getopt_long(argc, argv, "p:cs", long_options, NULL)) != -1) {
-        switch (opt) {
-            case 'p': port_number = atoi(optarg); break;
-            default: fprintf(stderr, "Usage: %s [-p port]\n", argv[0]); return -1;
-        }
-    }
-    
-    return 0;
-}
-
-int server_callback(void *args) {
+int server_blue_callback(void *args) {
     char buffer[1024];
     struct sockaddr_in client_address;
     socklen_t client_address_length = sizeof(client_address);
@@ -44,13 +26,5 @@ int server_callback(void *args) {
 
 int server_blue(port_number_t port_number) {
     socket_fd_t server_socket = initialize_server(port_number);
-    return server_socket < 0 || start_server(server_socket, server_callback, NULL) < 0 ? -1 : 0;
-}
-
-int main(int argc, char *argv[]) {
-    parse_arguments(argc, argv);
-    if (server_blue(port_number) > -1)
-        return 0;
-    fprintf(stderr, "Error: %s\n", strerror(errno));
-    return -1;
+    return server_socket < 0 || start_server(server_socket, server_blue_callback, NULL) < 0 ? -1 : 0;
 }
