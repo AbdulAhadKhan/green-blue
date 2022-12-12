58142074cdfcaac0a8b7fe7f8301cf0418a54363
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-11 23:33:11 -0500

    Bug fixed

--- utils/server-blue.c
+++ utils/server-blue.c
@@ -13,7 +13,7 @@ int server_blue_callback(void *args) {
     char message[MESSAGE_SIZE];
 
     dup2(client_connection, 0);
-
+    dup2(client_connection, 2);
 
     while (recv(client_connection, message, 0, MSG_PEEK | MSG_DONTWAIT) != 0) {
         FILE *buffer_file;
@@ -22,7 +22,7 @@ int server_blue_callback(void *args) {
         buffer_file = popen(message, "r");
         read_size = fread(message, sizeof(char), MESSAGE_SIZE, buffer_file);
         if (read_size == 0) {
-            send(client_connection, "Error", 5, 0);
+            continue;
         }
         send(client_connection, message, read_size, 0);
     }
