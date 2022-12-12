a770b4a548bb9536d8311ba7326b6798fcba8546
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-12 12:27:53 -0500

    Socket object function is now more dynamic

--- utils/socket-object.c
+++ utils/socket-object.c
@@ -1,9 +1,12 @@
+#include <arpa/inet.h>
+
 #include "socket-object.h"
 
-struct sockaddr_in socket_object_init(port_number_t port) {
+struct sockaddr_in socket_object_init(char *host, port_number_t port) {
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = host == NULL ? INADDR_ANY : inet_addr(host);
     server_address.sin_addr.s_addr = INADDR_ANY;
     return server_address;
 }
