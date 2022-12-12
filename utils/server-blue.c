7cd0a084fcbe910ea260137e65901ee879f6d8ba
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-11 23:27:30 -0500

    Not halting on exec error

--- utils/server-blue.c
+++ utils/server-blue.c
@@ -1,15 +1,19 @@
 #include <arpa/inet.h>
 #include <string.h>
+#include <wait.h>
+#include <sys/stat.h>
 
 #include "utils.h"
 #include "server-blue.h"
 #include "ANSI-colors.h"
 
 int server_blue_callback(void *args) {
+    int status;
     int read_size;
     char message[MESSAGE_SIZE];
 
     dup2(client_connection, 0);
+
 
     while (recv(client_connection, message, 0, MSG_PEEK | MSG_DONTWAIT) != 0) {
         FILE *buffer_file;
@@ -17,6 +21,9 @@ int server_blue_callback(void *args) {
         message[read_size] = '\0';
         buffer_file = popen(message, "r");
         read_size = fread(message, sizeof(char), MESSAGE_SIZE, buffer_file);
+        if (read_size == 0) {
+            send(client_connection, "Error", 5, 0);
+        }
         send(client_connection, message, read_size, 0);
     }
 
