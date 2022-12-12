a83f12db5b1c79510119fc16e7a7064427e65f81
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2022-12-12 12:57:06 -0500

    Added docs, moved functions in order of execution and renamed function

--- green-client.c
+++ green-client.c
@@ -6,10 +6,7 @@
 #include <signal.h>
 
 #include <pthread.h>
-#include <sys/mman.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
 #include "utils/meta.h"
 #include "utils/ANSI-colors.h"
@@ -33,6 +30,47 @@ static struct option long_options[] = {
     {0,              0,                  0,  0}
 };
 
+/**
+ * @brief Invoked when the program is given a SIGINT signal.
+ * Closes the server socket and exits the program.
+ * 
+ * @param sig 
+ */
+void interrupt_handler(int sig) {
+    printf("%s[-]%s Disconnecting from server: %s:%d\n", 
+            ANSI_COLOR_RED, ANSI_COLOR_RESET, configs.address, configs.server_port);
+    close(server_socket);
+    exit(0);
+}
+
+/**
+ * @brief Thread function that checks if the connection is still alive
+ * every second. If the connection is closed, the program is given a
+ * SIGINT signal.
+ * 
+ * @param args 
+ * @return void* 
+ */
+void * check_connection_thread(void *args) {
+    socket_fd_t connection = *(socket_fd_t *) args;
+    
+    while (recv(connection, NULL, 0, MSG_PEEK | MSG_DONTWAIT) != 0) sleep(1);
+    
+    printf("%s\n[-]%s Connection closed by server\n", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+    kill(getpid(), SIGINT);
+
+    return NULL;
+}
+
+/**
+ * @brief Parse the arguments passed to the program and set the
+ * configuration variables.
+ * 
+ * @param argc 
+ * @param argv 
+ * @param config 
+ * @return int 
+ */
 int parse_arguments(int argc, char *argv[], struct config *config) {
     int opt;
 
@@ -47,23 +85,13 @@ int parse_arguments(int argc, char *argv[], struct config *config) {
     return 0;
 }
 
-
-void * check_connection_thread(void *args) {
-    socket_fd_t connection = *(socket_fd_t *) args;
-    
-    while (recv(connection, NULL, 0, MSG_PEEK | MSG_DONTWAIT) != 0) sleep(1);
-    
-    printf("%s\n[-]%s Connection closed by server\n", ANSI_COLOR_RED, ANSI_COLOR_RESET);
-    exit(0);
-}
-
-void quit(int sig) {
-    printf("%s[-]%s Disconnecting from server: %s:%d\n", 
-            ANSI_COLOR_RED, ANSI_COLOR_RESET, configs.address, configs.server_port);
-    close(server_socket);
-    exit(0);
-}
-
+/**
+ * @brief Get the message from the user and send it to the server.
+ * Then, get the response from the server and print it. If the user
+ * types "quit", the program will be terminated by sending a SIGINT signal.
+ * 
+ * @param socket: socket file descriptor (socket_fd_t)
+ */
 void process_communication(socket_fd_t socket) {
     int read_size;
     char buffer[MESSAGE_SIZE];
@@ -80,6 +108,12 @@ void process_communication(socket_fd_t socket) {
     }
 }
 
+/**
+ * @brief Create a client socket and connect to the server.
+ * If the connection is successful, start a thread to check the connection status
+ * and start the communication process. Otherwise, print an error message and return.
+ * 
+ */
 void green_client() {
     pthread_t thread;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -87,7 +121,8 @@ void green_client() {
 
     if (connect(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) != -1) {
         pthread_create(&thread, NULL, check_connection_thread, &server_socket);
-        printf("%s[+]%s Connected to server: %s:%d\n", ANSI_COLOR_GREEN, ANSI_COLOR_RESET, configs.address, configs.server_port);
+        printf("%s[+]%s Connected to server: %s:%d\n", ANSI_COLOR_GREEN, 
+               ANSI_COLOR_RESET, configs.address, configs.server_port);
         process_communication(server_socket);        
     }
 
@@ -95,8 +130,15 @@ void green_client() {
             ANSI_COLOR_RED, ANSI_COLOR_RESET, configs.address, configs.server_port);
 }
 
+/**
+ * @brief Start the client application
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char *argv[]) {
-    signal(SIGINT, quit);
+    signal(SIGINT, interrupt_handler);
     parse_arguments(argc, argv, &configs);
     green_client();
 }
